effb719d78987e7b22d878e020c199e3a72c3004
Gaurav Parthasarathy <gauravp@ethz.ch>
2019-12-19 17:18:37 +0100

    progress Lang

--- BoogieLang/Lang.thy
+++ BoogieLang/Lang.thy
@@ -3,26 +3,34 @@ theory Lang
 begin
 
 type_synonym fname = string (* function name *)
-type_synonym vname = string (*variable name *)
+type_synonym vname = string (* variable name *)
+type_synonym mname = string (* method name *)
 
-datatype val =  Bool bool  | Int int
+datatype val =  Bool bool  | Intg int
 
-datatype bop = Eq | Add | Le 
+datatype binop = Eq | Add | Le | And
 datatype unop = Not
 
 datatype ty
   = Boolean |
     Integer
 
+type_synonym fdecl = "fname \<times> ty list \<times> ty"
+(*for now just support method without return type and some body *)
+type_synonym 'm mdecl = "mname \<times> ty list \<times> 'm" 
+
+(*for now just support a single method and no axioms*)
+datatype 'm prog = Program "fdecl list" "'m mdecl" 
+
 datatype expr
   = Var vname
   | Val val
-  | BinOp "( expr)" bop "(expr)" ("_ \<guillemotleft>_\<guillemotright> _" [80,0,81] 80) 
+  | BinOp "(expr)" binop "(expr)" ("_ \<guillemotleft>_\<guillemotright> _" [80,0,81] 80) 
   | FunExp fname "(expr list)"
 
 datatype cmd
  = Assert expr
  | Assume expr
- | Seq expr expr ("_;;/ _"             [61,60]60)
+ | Seq expr expr ("_;;/ _"             [61,60]60) 
 
 end

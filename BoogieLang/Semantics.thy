f065759bde1c87081717aa824a7fbfe673088352
Gaurav Parthasarathy <gauravp@ethz.ch>
2020-01-17 14:48:45 +0100

    expression reduction, cmd reduction

--- BoogieLang/Semantics.thy
+++ BoogieLang/Semantics.thy
@@ -2,24 +2,31 @@ theory Semantics
 imports Lang CFG
 begin
 
-datatype state = Normal "vname \<rightharpoonup> val" | Failure
+type_synonym nstate = "vname \<rightharpoonup> val"
+
+datatype state = Normal nstate | Failure | Magic
 
 (* define context *)
-datatype prog_context = Ctxt "fname \<rightharpoonup> (val list \<Rightarrow> val list)"
+type_synonym fun_interp = "fname \<rightharpoonup> (val list \<Rightarrow> val)"
+
+(* type declarations *)
+type_synonym fun_decl = "fname \<rightharpoonup> (ty list \<times> ty)"
+
+type_synonym fun_context = "fun_decl \<times> fun_interp"
 
 fun binop_lessThan :: "val \<Rightarrow>  val \<rightharpoonup> val"
 where
-  "binop_lessThan (Intg i1) (Intg i2) = Some (Bool (i1 < i2))"
+  "binop_lessThan (IntV i1) (IntV i2) = Some (BoolV (i1 < i2))"
 | "binop_lessThan _ _ = None"
 
 fun binop_add :: "val \<Rightarrow> val \<rightharpoonup> val"
   where 
-    "binop_add (Intg i1) (Intg i2) = Some (Intg (i1 + i2))"
+    "binop_add (IntV i1) (IntV i2) = Some (IntV (i1 + i2))"
   | "binop_add _ _ = None"
    
 fun binop_and :: "val \<Rightarrow> val \<rightharpoonup> val"
   where
-    "binop_and (Bool b1) (Bool b2) = Some (Bool (b1 \<and> b2))"
+    "binop_and (BoolV b1) (BoolV b2) = Some (BoolV (b1 \<and> b2))"
   | "binop_and _ _ = None"
 
 (*
@@ -31,31 +38,56 @@ fun feval :: "'m prog \<Rightarrow> prog_context \<Rightarrow> fname  \<Rightarr
 
 fun binop_eval ::"binop \<Rightarrow> val \<Rightarrow> val \<rightharpoonup> val"
   where
-   "binop_eval Eq v1 v2 = Some (Bool (v1 = v2))"
+   "binop_eval Eq v1 v2 = Some (BoolV (v1 = v2))"
  | "binop_eval Add v1 v2 = binop_add v1 v2"
  | "binop_eval Le v1 v2 = binop_lessThan v1 v2"
  | "binop_eval And v1 v2 = binop_and v1 v2"
 
 
-(*
-  = Var vname
-  | Val val
-  | BinOp "(expr)" binop "(expr)" ("_ \<guillemotleft>_\<guillemotright> _" [80,0,81] 80) 
-  | FunExp fname "(expr list)"
-*)
-
-fun expr_eval :: "prog_context \<Rightarrow> expr \<Rightarrow> state \<rightharpoonup> val"
+inductive red_expr :: "fun_context \<Rightarrow> expr \<Rightarrow> nstate \<Rightarrow> val \<Rightarrow> bool"
+  ("_ \<turnstile> ((\<langle>_,_\<rangle>) \<Down> _)" [81,81,81] 99)
+  and red_exprs :: "fun_context \<Rightarrow> expr list \<Rightarrow> nstate \<Rightarrow> val list \<Rightarrow> bool"
+  ("_ \<turnstile> ((\<langle>_,_\<rangle>) [\<Down>] _)" [81,81,81] 99)
+  for \<Gamma> :: fun_context
   where 
-    "expr_eval \<Gamma> (Var x) s = s(x)"
-  | "expr_eval \<Gamma> (Val v ) s = v"
-  | "expr_eval \<Gamma> (e1 \<guillemotleft>op\<guillemotright> e2) s = binop_eval op (expr_eval \<Gamma> e1 s) (expr_eval \<Gamma> e2 s)"
-  | "FunExp fn vs = _"
+    RedVar: "\<lbrakk> n_s(x) = Some v \<rbrakk> \<Longrightarrow> \<Gamma> \<turnstile> \<langle>(Var x), n_s\<rangle> \<Down> v"
+  | RedVal: "\<Gamma> \<turnstile> \<langle>(Val v), n_s\<rangle> \<Down> v"
+  | RedBinOp: "\<lbrakk> \<Gamma> \<turnstile>\<langle>e1, n_s\<rangle> \<Down> v1; \<Gamma> \<turnstile> \<langle>e1, n_s\<rangle> \<Down> v2;
+                 binop_eval bop v1 v2 = (Some v) \<rbrakk> \<Longrightarrow> 
+             \<Gamma> \<turnstile> \<langle>(e1 \<guillemotleft>bop\<guillemotright> e2), n_s\<rangle> \<Down> v"
+  (* a function application only reduces if the arguments have the expected types *)
+  | RedFunOp: "\<lbrakk> \<Gamma> \<turnstile> \<langle>args, n_s\<rangle> [\<Down>] v_args; 
+             (fst \<Gamma>) f = Some (ty_args, _);
+             (snd \<Gamma>) f = Some f_interp;
+             ty_args = map ty_of_val v_args;
+             v = f_interp v_args \<rbrakk> \<Longrightarrow>
+             \<Gamma> \<turnstile> \<langle> FunExp f args, n_s \<rangle> \<Down> v"
+  | RedListNil:
+    "\<Gamma> \<turnstile> \<langle>[], n_s\<rangle> [\<Down>] []"
+  | RedListCons:
+    "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> v; \<Gamma> \<turnstile> \<langle>es, n_s\<rangle> [\<Down>] vs \<rbrakk> \<Longrightarrow>
+      \<Gamma> \<turnstile> \<langle>(e # es), n_s\<rangle> [\<Down>] (v # vs)"
 
-
-inductive instr_step :: "prog_context \<Rightarrow> cmd \<Rightarrow> state \<Rightarrow> state \<Rightarrow> bool"
-  ("_\<turnstile> ((_,_) \<rightarrow>/ _)" [81,81,81] 100)
-  for \<Gamma> :: prog_context
+inductive red_cmd :: "fun_context \<Rightarrow> cmd \<Rightarrow> state \<Rightarrow> state \<Rightarrow> bool"
+  ("_ \<turnstile> ((\<langle>_,_\<rangle>) \<rightarrow>/ _)" [81,81,81] 99)
+  and red_cmds :: "fun_context \<Rightarrow> cmd list \<Rightarrow> state \<Rightarrow> state \<Rightarrow> bool"
+  ("_ \<turnstile> ((\<langle>_,_\<rangle>) [\<rightarrow>]/ _)" [81,81,81] 99)
+  for \<Gamma> :: fun_context
   where
-    Assert: "\<lbrakk>\<Gamma> \<turnstile> (Assert e, s) \<rightarrow> s"
+    RedAssertOk: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV true) \<rbrakk> \<Longrightarrow> 
+                 \<Gamma> \<turnstile> \<langle>Assert e, Normal n_s\<rangle> \<rightarrow> Normal n_s"
+  | RedAssertFail: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV false) \<rbrakk> \<Longrightarrow> 
+                   \<Gamma> \<turnstile> \<langle>Assert e, Normal n_s\<rangle> \<rightarrow> Failure"
+  | RedAssumeOk: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV true) \<rbrakk> \<Longrightarrow> 
+                 \<Gamma> \<turnstile> \<langle>Assume e, Normal n_s\<rangle> \<rightarrow> Normal n_s"
+  | RedAssumeMagic: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV false) \<rbrakk> \<Longrightarrow> 
+                 \<Gamma> \<turnstile> \<langle>Assume e, Normal n_s\<rangle> \<rightarrow> Magic"
+  | RedPropagateMagic: "\<Gamma> \<turnstile> \<langle>s, Magic\<rangle> \<rightarrow> Magic"
+  | RedPropagateFailure: "\<Gamma> \<turnstile> \<langle>s, Failure\<rangle> \<rightarrow> Failure"
+  | RedListSingle: "\<Gamma> \<turnstile> \<langle>c,s\<rangle> \<rightarrow> s' \<Longrightarrow> \<Gamma> \<turnstile> \<langle>[c],s\<rangle> [\<rightarrow>] s'"
+  | RedListCons: "\<lbrakk> \<Gamma> \<turnstile> \<langle>c,s\<rangle> \<rightarrow> s'; \<Gamma> \<turnstile> \<langle>cs,s'\<rangle> [\<rightarrow>] s'' \<rbrakk> \<Longrightarrow> 
+                    \<Gamma> \<turnstile> \<langle>(c # cs), s\<rangle> [\<rightarrow>] s''"
+                
+
 
 end

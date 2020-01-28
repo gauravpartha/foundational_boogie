36e88f5b386aba0e036c3fd56ef8c3c5918b0754
Gaurav Parthasarathy <gauravp@ethz.ch>
2020-01-28 09:31:25 +0100

    fixes to semantics

--- BoogieLang/Semantics.thy
+++ BoogieLang/Semantics.thy
@@ -14,10 +14,10 @@ type_synonym fun_interp = "fname \<rightharpoonup> (val list \<rightharpoonup> v
 
 type_synonym fun_context = "fdecl list \<times> fun_interp"
 
-fun binop_lessThan :: "val \<Rightarrow> val \<rightharpoonup> val"
+fun binop_lessOrEqual :: "val \<Rightarrow> val \<rightharpoonup> val"
 where
-  "binop_lessThan (IntV i1) (IntV i2) = Some (BoolV (i1 < i2))"
-| "binop_lessThan _ _ = None"
+  "binop_lessOrEqual (IntV i1) (IntV i2) = Some (BoolV (i1 \<le> i2))"
+| "binop_lessOrEqual _ _ = None"
 
 fun binop_add :: "val \<Rightarrow> val \<rightharpoonup> val"
   where 
@@ -33,7 +33,7 @@ fun binop_eval ::"binop \<Rightarrow> val \<Rightarrow> val \<rightharpoonup> va
   where
    "binop_eval Eq v1 v2 = Some (BoolV (v1 = v2))"
  | "binop_eval Add v1 v2 = binop_add v1 v2"
- | "binop_eval Le v1 v2 = binop_lessThan v1 v2"
+ | "binop_eval Le v1 v2 = binop_lessOrEqual v1 v2"
  | "binop_eval And v1 v2 = binop_and v1 v2"
 
 (* big-step *)
@@ -45,7 +45,7 @@ inductive red_expr :: "fun_context \<Rightarrow> expr \<Rightarrow> nstate \<Rig
   where 
     RedVar: "\<lbrakk> n_s(x) = Some v \<rbrakk> \<Longrightarrow> \<Gamma> \<turnstile> \<langle>(Var x), n_s\<rangle> \<Down> v"
   | RedVal: "\<Gamma> \<turnstile> \<langle>(Val v), n_s\<rangle> \<Down> v"
-  | RedBinOp: "\<lbrakk> \<Gamma> \<turnstile>\<langle>e1, n_s\<rangle> \<Down> v1; \<Gamma> \<turnstile> \<langle>e1, n_s\<rangle> \<Down> v2;
+  | RedBinOp: "\<lbrakk> \<Gamma> \<turnstile>\<langle>e1, n_s\<rangle> \<Down> v1; \<Gamma> \<turnstile> \<langle>e2, n_s\<rangle> \<Down> v2;
                  binop_eval bop v1 v2 = (Some v) \<rbrakk> \<Longrightarrow> 
              \<Gamma> \<turnstile> \<langle>(e1 \<guillemotleft>bop\<guillemotright> e2), n_s\<rangle> \<Down> v"
   (* a function application only reduces if the arguments have the expected types *)
@@ -53,7 +53,8 @@ inductive red_expr :: "fun_context \<Rightarrow> expr \<Rightarrow> nstate \<Rig
              (snd \<Gamma>) f = Some f_interp;
              ty_args = map type_of_val v_args;*)
   | RedFunOp: "\<lbrakk> \<Gamma> \<turnstile> \<langle>args, n_s\<rangle> [\<Down>] v_args; 
-             f_interp v_args = Some v \<rbrakk> \<Longrightarrow>
+                (snd \<Gamma>) f = Some f_interp;
+                f_interp v_args = Some v \<rbrakk> \<Longrightarrow>
              \<Gamma> \<turnstile> \<langle> FunExp f args, n_s \<rangle> \<Down> v"
   | RedListNil:
     "\<Gamma> \<turnstile> \<langle>[], n_s\<rangle> [\<Down>] []"
@@ -67,20 +68,26 @@ inductive red_cmd :: "fun_context \<Rightarrow> cmd \<Rightarrow> state \<Righta
   ("_ \<turnstile> ((\<langle>_,_\<rangle>) [\<rightarrow>]/ _)" [81,81,81] 99)
   for \<Gamma> :: fun_context
   where
-    RedAssertOk: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV true) \<rbrakk> \<Longrightarrow> 
+    RedAssertOk: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV True) \<rbrakk> \<Longrightarrow> 
                  \<Gamma> \<turnstile> \<langle>Assert e, Normal n_s\<rangle> \<rightarrow> Normal n_s"
-  | RedAssertFail: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV false) \<rbrakk> \<Longrightarrow> 
+  | RedAssertFail: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV False) \<rbrakk> \<Longrightarrow> 
                    \<Gamma> \<turnstile> \<langle>Assert e, Normal n_s\<rangle> \<rightarrow> Failure"
-  | RedAssumeOk: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV true) \<rbrakk> \<Longrightarrow> 
+  | RedAssumeOk: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV True) \<rbrakk> \<Longrightarrow> 
                  \<Gamma> \<turnstile> \<langle>Assume e, Normal n_s\<rangle> \<rightarrow> Normal n_s"
-  | RedAssumeMagic: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV false) \<rbrakk> \<Longrightarrow> 
+  | RedAssumeMagic: "\<lbrakk> \<Gamma> \<turnstile> \<langle>e, n_s\<rangle> \<Down> (BoolV False) \<rbrakk> \<Longrightarrow> 
                  \<Gamma> \<turnstile> \<langle>Assume e, Normal n_s\<rangle> \<rightarrow> Magic"
   | RedPropagateMagic: "\<Gamma> \<turnstile> \<langle>s, Magic\<rangle> \<rightarrow> Magic"
   | RedPropagateFailure: "\<Gamma> \<turnstile> \<langle>s, Failure\<rangle> \<rightarrow> Failure"
-  | RedListSingle: "\<Gamma> \<turnstile> \<langle>c,s\<rangle> \<rightarrow> s' \<Longrightarrow> \<Gamma> \<turnstile> \<langle>[c],s\<rangle> [\<rightarrow>] s'"
+  | RedListNil: "\<Gamma> \<turnstile> \<langle>[],s\<rangle> [\<rightarrow>] s"
   | RedListCons: "\<lbrakk> \<Gamma> \<turnstile> \<langle>c,s\<rangle> \<rightarrow> s'; \<Gamma> \<turnstile> \<langle>cs,s'\<rangle> [\<rightarrow>] s'' \<rbrakk> \<Longrightarrow> 
                     \<Gamma> \<turnstile> \<langle>(c # cs), s\<rangle> [\<rightarrow>] s''"
 
+(* configurations on the left and right are not the same
+   left configuration: \<langle>Node, State\<rangle>
+   right configuration:  
+               (1) \<langle>inl Node, State\<rangle> [if move to another node]
+               (2) \<langle>inr (), State\<rangle> [if no outgoing node, i.e., return]
+*)
 inductive red_cfg :: "fun_context \<Rightarrow> mbodyCFG \<Rightarrow> node \<Rightarrow> state \<Rightarrow> (node + unit) \<Rightarrow> state \<Rightarrow> bool"
   ("_,_ \<turnstile> ((\<langle>_,_\<rangle>) -n\<rightarrow>/ (\<langle>_,_\<rangle>))" [81,81,81] 99)
   and red_cfg_multi :: "fun_context \<Rightarrow> mbodyCFG \<Rightarrow> node \<Rightarrow> state \<Rightarrow> (node + unit) \<Rightarrow> state \<Rightarrow> bool"
@@ -88,12 +95,14 @@ inductive red_cfg :: "fun_context \<Rightarrow> mbodyCFG \<Rightarrow> node \<Ri
   for \<Gamma> :: fun_context and G :: mbodyCFG
   where
     RedNode: "\<lbrakk>node_to_block(G) n = Some cs; \<Gamma> \<turnstile> \<langle>cs,s\<rangle> [\<rightarrow>] s'; n' \<in> out_edges(G) n  \<rbrakk> \<Longrightarrow> 
-               \<Gamma>, G  \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow> \<langle>inl n',s'\<rangle>"
+               \<Gamma>, G  \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow> \<langle>Inl n',s'\<rangle>"
   | RedReturn: "\<lbrakk>node_to_block(G) n = Some cs; \<Gamma> \<turnstile> \<langle>cs,s\<rangle> [\<rightarrow>] s'; (out_edges(G) n) = {} \<rbrakk> \<Longrightarrow> 
-               \<Gamma>, G  \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow> \<langle>inr (),s'\<rangle>" 
-  | RedMultiStep1: "\<Gamma>, G \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow>* \<langle>inr n,s'\<rangle>"
-  | RedMultiStep2: "\<lbrakk> \<Gamma>, G \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow> \<langle>inl n',s'\<rangle>; \<Gamma>, G \<turnstile> \<langle>n', s'\<rangle> -n\<rightarrow>* \<langle>nu, s''\<rangle> \<rbrakk> \<Longrightarrow>
+               \<Gamma>, G  \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow> \<langle>Inr (),s'\<rangle>" 
+  | RedMultiStep1: "\<Gamma>, G \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow>* \<langle>Inl n, s\<rangle>"
+  | RedMultiStep2: "\<lbrakk> \<Gamma>, G \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow> \<langle>Inl n',s'\<rangle>; \<Gamma>, G \<turnstile> \<langle>n', s'\<rangle> -n\<rightarrow>* \<langle>nu, s''\<rangle> \<rbrakk> \<Longrightarrow>
                 \<Gamma>, G \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow>* \<langle>nu, s''\<rangle>"
+  | RedMultiStep3: "\<lbrakk> \<Gamma>, G \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow> \<langle>Inr (),s'\<rangle> \<rbrakk> \<Longrightarrow>
+                \<Gamma>, G \<turnstile> \<langle>n,s\<rangle> -n\<rightarrow>* \<langle>Inr (), s'\<rangle>"
 
 fun fun_interp_single_wf :: "fdecl \<Rightarrow> (val list \<rightharpoonup> val) \<Rightarrow> bool"
   where "fun_interp_single_wf (_, args_ty, ret_ty) f =

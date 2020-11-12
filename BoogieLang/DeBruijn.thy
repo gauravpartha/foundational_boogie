bc10950b59c783bb84e09700e2ce86d664c91700
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2020-11-12 14:46:25 +0100

    update DeBruijn to reflect state model change

--- BoogieLang/DeBruijn.thy
+++ BoogieLang/DeBruijn.thy
@@ -16,11 +16,13 @@ where
 
 primrec shift :: "nat \<Rightarrow> nat \<Rightarrow> expr \<Rightarrow> expr" ("\<up>")
 where
-  "\<up> n k (Var i) = (if i < k then Var i else Var (i + n))"
+  "\<up> n k (Var i) = Var i"
+| "\<up> n k (BVar i) = (if i < k then BVar i else BVar (i + n))"
 | "\<up> n k (Lit l) = (Lit l)"
 | "\<up> n k (UnOp uop e) = UnOp uop (\<up> n k e)"
 | "\<up> n k (e1 \<guillemotleft>bop\<guillemotright> e2) = (\<up> n k e1) \<guillemotleft>bop\<guillemotright> (\<up> n k e2)"
 | "\<up> n k (FunExp f ty_args args) = FunExp f ty_args (map (\<up> n k) args)"
+| "\<up> n k (Old e) = Old (\<up> n k e)"
 | "\<up> n k (Forall ty e) = (Forall ty (\<up> n (k+1) e))"
 | "\<up> n k (Exists ty e) = (Exists ty (\<up> n (k+1) e))"
 | "\<up> n k (ForallT e) = (ForallT (\<up> n k e))"
@@ -32,10 +34,12 @@ fun shift_env :: "nat \<Rightarrow> nat \<Rightarrow> (nat \<rightharpoonup> ty)
 primrec shift_ty_term :: "nat \<Rightarrow> nat \<Rightarrow> expr \<Rightarrow> expr" ("\<up>\<^sub>\<tau>")
   where
   "\<up>\<^sub>\<tau> n k (Var i) = Var i"
+| "\<up>\<^sub>\<tau> n k (BVar i) = BVar i"
 | "\<up>\<^sub>\<tau> n k (Lit l) = (Lit l)"
 | "\<up>\<^sub>\<tau> n k (UnOp uop e) = UnOp uop (\<up>\<^sub>\<tau> n k e)"
 | "\<up>\<^sub>\<tau> n k (e1 \<guillemotleft>bop\<guillemotright> e2) = (\<up>\<^sub>\<tau> n k e1) \<guillemotleft>bop\<guillemotright> (\<up>\<^sub>\<tau> n k e2)"
 | "\<up>\<^sub>\<tau> n k (FunExp f ty_args args) = FunExp f (map (shiftT n k) ty_args) (map (\<up>\<^sub>\<tau> n k) args)"
+| "\<up>\<^sub>\<tau> n k (Old e) = Old (\<up>\<^sub>\<tau> n k e)"
 | "\<up>\<^sub>\<tau> n k (Forall ty e) = (Forall (shiftT n k ty) (\<up>\<^sub>\<tau> n k e))"
 | "\<up>\<^sub>\<tau> n k (Exists ty e) = (Exists (shiftT n k ty) (\<up>\<^sub>\<tau> n k e))"
 | "\<up>\<^sub>\<tau> n k (ForallT e) = (ForallT (\<up>\<^sub>\<tau> n (k+1) e))"
@@ -54,10 +58,12 @@ S must be shifted by j if variable j must be substituted.\<close>
 primrec subst_ty_expr :: "expr \<Rightarrow> nat \<Rightarrow> ty \<Rightarrow> expr" ("_[_ \<mapsto>\<^sub>\<tau> _]" [300, 0, 0] 300)
   where
     "(Var i)[k \<mapsto>\<^sub>\<tau> S] = Var i"
+  | "(BVar i)[k \<mapsto>\<^sub>\<tau> S] = BVar i"
   | "(Lit l)[k \<mapsto>\<^sub>\<tau> S] = (Lit l)"
   | "(UnOp uop e)[k \<mapsto>\<^sub>\<tau> S] = UnOp uop (e[k \<mapsto>\<^sub>\<tau> S])"
   | "(e1 \<guillemotleft>bop\<guillemotright> e2)[k \<mapsto>\<^sub>\<tau> S] = (e1[k \<mapsto>\<^sub>\<tau> S]) \<guillemotleft>bop\<guillemotright> (e2[k \<mapsto>\<^sub>\<tau> S])"
   | "(FunExp f ty_args args)[k \<mapsto>\<^sub>\<tau> S] = FunExp f (map (\<lambda>t. t[k \<mapsto>\<^sub>\<tau> S]\<^sub>\<tau>) ty_args) (map (\<lambda>e. e[k \<mapsto>\<^sub>\<tau> S]) args)"
+  | "(Old e)[k \<mapsto>\<^sub>\<tau> S] = Old (e[k \<mapsto>\<^sub>\<tau> S])"
   | "(Forall ty e)[k \<mapsto>\<^sub>\<tau> S] = (Forall (ty[k \<mapsto>\<^sub>\<tau> S]\<^sub>\<tau>) (e[k \<mapsto>\<^sub>\<tau> S]))"
   | "(Exists ty e)[k \<mapsto>\<^sub>\<tau> S] = (Exists (ty[k \<mapsto>\<^sub>\<tau> S]\<^sub>\<tau>) (e[k \<mapsto>\<^sub>\<tau> S]))"
   | "(ForallT e)[k \<mapsto>\<^sub>\<tau> S] = (ForallT (e[k+1 \<mapsto>\<^sub>\<tau> S]))"
@@ -226,10 +232,4 @@ lemma msubst_ty_existsT:
 "msubst_ty_expr ts (ExistsT e) = ExistsT (fold (\<lambda> \<tau> e'. (e'[1 \<mapsto>\<^sub>\<tau> \<tau>])) ts e)"
   by (induction ts arbitrary: e; auto)
 
-
-(*
-primrec subst :: "expr \<Rightarrow> nat \<Rightarrow> expr \<Rightarrow> expr" ("_[_ \<mapsto> _]" [300, 0, 0] 300)
-  where
-    "(Var i)[k \<mapsto> e] = (if k < i then Var (i-1) else if i = k then \<up> k 0 e else Var i)"
-*)
 end
